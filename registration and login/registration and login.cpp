ed5c2ccd557fe578fffac229370bda3cbd1b362f
Sathira Pramudith <sathirapramudith1@gmail.com>
2025-10-30 07:53:31 +0530

    cout a data output data

--- registration and login/registration and login.cpp
+++ registration and login/registration and login.cpp
@@ -3,5 +3,12 @@
 using namespace std;
 
 int main() {
+	char choice;
+	cout << "\n1 Login";
+	cout << "\n2 Sign up";
+	cout << "\n3 Forget Password";
+	cout << "\n4 Exit";
+	cout << "\n5 Enter your choice :: ";
+	cin >> choice;
 
 }
